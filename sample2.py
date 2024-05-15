946dd1a125595894e4b643ffc386db66fee025b1
abelbabu <97331744+abelbabu@users.noreply.github.com>
2024-05-15 11:21:04 +0530

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,13 @@
+"""#print("enter your name")
+#x=input()
+#print("NAME :",x)
+a=int(input("Enter mark1"))
+b=int(input("Enter mark2"))
+print("Sum= ",a+b)
+"""
+cource='english'
+print(cource)
+print(cource[0])
+print(cource[1])
+print(cource[0:4])
+             
